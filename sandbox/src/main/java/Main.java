56f0af788186013971d93bd97f659d46ef2ec09e
annazuzu <maxann89@gmail.com>
2020-02-07 00:36:47 +0300

    Создан проект "Песочница"
    5 файлов получилось потому, что сначала профакапилась и скачала gradle-all, а не gradle-bin, ещё и файл .gradle открыла Intellijidea - она у меня начала автоматически компилироваться с помощью gradle и искать git-репозиторий, которого нет. В общем, я исправилась, но файлов получилось много.

---
+++ sandbox/src/main/java/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(convertCelsiusToFahrenheit(41));
+    }
+
+    public static double convertCelsiusToFahrenheit(int celsius) {
+        double fahrenheit = (9.0/5.0)*celsius + 32;
+        return fahrenheit;
+    }
+}
+
