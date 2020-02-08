f9a82a42e4072ee1826d5777f84d8e84d90ecea1
annazuzu <maxann89@gmail.com>
2020-02-08 20:18:41 +0300

    Создан пакет для моей первой программы

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -1,4 +1,6 @@
-public class Main {
+package ru.stqa.pft.sandbox;
+
+public class MyFirstProgram {
 
     public static void main(String[] args) {
         System.out.println(convertCelsiusToFahrenheit(41));
