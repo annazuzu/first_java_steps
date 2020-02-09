81885b71db47360abd5417c2fac13a2dc5cb01a7
annazuzu <maxann89@gmail.com>
2020-02-09 23:15:04 +0300

    Редактирование MyFirstProgram.java

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,12 +3,13 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
 
     public static void main(String[] args) {
-        System.out.println(convertCelsiusToFahrenheit(41));
-    }
+        Square s = new Square(5);
+        System.out.println("Площадь квадрата со стороной " + s.l + " - " + s.area());
 
-    public static double convertCelsiusToFahrenheit(int celsius) {
-        double fahrenheit = (9.0/5.0)*celsius + 32;
-        return fahrenheit;
+        Rectangle r = new Rectangle(4,6);
+        System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " - " + r.area());
+
     }
+    
 }
 
