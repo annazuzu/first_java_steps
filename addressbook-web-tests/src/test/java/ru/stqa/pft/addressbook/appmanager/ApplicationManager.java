d758d8414af35973f357e9ca24f30d50a3e3f66a
annazuzu <maxann89@gmail.com>
2020-02-16 20:26:26 +0300

    Лекция 2.6. Заменяем наследование на делегирование и создаём двухуровневую архитектуру
    Вспомогательные функции раскидываются по нескольким дополнительным классам, более узкоспециализированным: 1. функции login,  logout; 2. функции для работы с контактами; 3. функции для работы с группами. Делаем п. 1 и 3.
    Это преобразование делается в 2 этапа:
    ----------------------------------------------
    1 этап: перенести все функции из базового класса TestBase во вспомогательный класс ApplicationManager. Заставить работать механизм делегирования вместо механизма наследования (клик пркн по названию класса > Refactor > Replace Inheritance with Delegation);
    2 этап: ApplicationManager делим на подклассы.
    ----------------------------------------------
    (После делегирования все обращения в в классах GroupCreationTests, GroupDeletionTests, TestBase идут через объектную ссылку app.<...>) ---> 1 слой - логика тестов; (основной код методов хранится в классе ApplicationManager) ---> 2 слой - логика взаимодействия с тестируемой системой.
    Для наглядности оформления данного архитектурного решения разнесли классы по разным пакетам внутри пакета addressbook.

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -1,28 +1,26 @@
-package ru.stqa.pft.addressbook;
+package ru.stqa.pft.addressbook.appmanager;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoAlertPresentException;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
+import ru.stqa.pft.addressbook.model.GroupData;
 
 import java.util.concurrent.TimeUnit;
 
-public class TestBase {
-    private WebDriver wd;
+public class ApplicationManager {
 
-    @BeforeMethod(alwaysRun = true)
-    public void setUp() throws Exception {
-      wd = new ChromeDriver();
-  //    baseUrl = "https://www.google.com/";
-      wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-      wd.get("http://localhost/addressbook/");
-      login("admin", "secret");
+    public WebDriver wd;
+
+    public void init() {
+        wd = new ChromeDriver();
+        wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        wd.get("http://localhost/addressbook/");
+        login("admin", "secret");
     }
 
-    private void login(String username, String password) {
+    public void login(String username, String password) {
       wd.findElement(By.name("user")).click();
       wd.findElement(By.name("user")).clear();
       wd.findElement(By.name("user")).sendKeys(username);
@@ -33,19 +31,19 @@ public class TestBase {
       wd.findElement(By.xpath("//input[@value='Login']")).click();
     }
 
-    protected void logout() {
+    public void logout() {
       wd.findElement(By.linkText("Logout")).click();
     }
 
-    protected void returntoGroupPage() {
+    public void returntoGroupPage() {
       wd.findElement(By.linkText("group page")).click();
     }
 
-    protected void submitGroupCreation() {
+    public void submitGroupCreation() {
       wd.findElement(By.name("submit")).click();
     }
 
-    protected void fillGroupForm(GroupData groupData) {
+    public void fillGroupForm(GroupData groupData) {
       wd.findElement(By.name("group_name")).click();
       wd.findElement(By.name("group_name")).clear();
       wd.findElement(By.name("group_name")).sendKeys(groupData.getName());
@@ -57,23 +55,19 @@ public class TestBase {
       wd.findElement(By.name("group_footer")).sendKeys(groupData.getFooter());
     }
 
-    protected void initGroupCreation() {
+    public void initGroupCreation() {
       wd.findElement(By.name("new")).click();
     }
 
-    protected void gotoGroupPage() {
+    public void gotoGroupPage() {
       wd.findElement(By.linkText("groups")).click();
     }
 
-    @AfterMethod(alwaysRun = true)
-    public void tearDown() throws Exception {
-      wd.quit();
-  //    String verificationErrorString = verificationErrors.toString();
-  //    if (!"".equals(verificationErrorString)) {
-  //      fail(verificationErrorString);
-      }
+    public void stop() {
+        wd.quit();
+    }
 
-    private boolean isElementPresent(By by) {
+    public boolean isElementPresent(By by) {
       try {
         wd.findElement(by);
         return true;
@@ -82,7 +76,7 @@ public class TestBase {
       }
     }
 
-    private boolean isAlertPresent() {
+    public boolean isAlertPresent() {
       try {
         wd.switchTo().alert();
         return true;
@@ -91,11 +85,11 @@ public class TestBase {
       }
     }
 
-    protected void deleteSelectedGroups() {
+    public void deleteSelectedGroups() {
       wd.findElement(By.name("delete")).click();
     }
 
-    protected void selectGroup() {
+    public void selectGroup() {
       wd.findElement(By.name("selected[]")).click();
     }
 }
