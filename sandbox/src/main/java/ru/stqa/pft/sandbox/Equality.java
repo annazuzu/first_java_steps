2f6d661c9f50147c24d1d1b92b89413270a45231
annazuzu <maxann89@gmail.com>
2020-02-23 13:38:57 +0300

    Лекция 3.1. - 3.4
    Реализовала запуск тестов с возможностью выбора в браузерах Firefox, Chrome, IE11. Исполняемые файлы драйверов для Firefox, Chrome лежат в директории "C:\Windows\System32", исполняемый файл драйвера IE11 положила в директорию, которая входит в окружение переменной PATH: "%JAVA_HOME%/bin".

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Equality.java
@@ -0,0 +1,17 @@
+package ru.stqa.pft.sandbox;
+
+public class Equality {
+    public static void main(String[] args) {
+        String s1 = "firefox 2.0";
+//        String s2 = new String(s1);
+//        String s2 = "firefox";
+//        String s2 = "fire" + "fox";
+        String s2 = "firefox " + Math.sqrt(4.0); //компилятор делает вычисления на этапе создания объекта, поэтому будут
+        // заведомо создаваться два разных объекта => метод equals(), сравнивающий значения двух объектов, выполнит сравнение
+        // с результатом "true", а "==" опять наткнется на два разных объекта и сравление будет ложным.
+
+        System.out.println(s1 == s2);
+        System.out.println(s1.equals(s2));
+
+    }
+}
