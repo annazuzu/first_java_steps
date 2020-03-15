ac7927bf3e02c996d39a74493706c1d37e7bfa20
annazuzu <maxann89@gmail.com>
2020-03-15 11:26:13 +0300

    Лекция 6.3 - 6.6(1/2)
    - Анализ опций командой строки при запуске генератора данных;
    - Параметризация тестовых методов;
    - Загрузка тестовых данных из файла;
    - Формат XML (1/2).

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/GroupDataGenerator.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/GroupDataGenerator.java
@@ -1,5 +1,10 @@
 package ru.stqa.pft.addressbook.generators;
 
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.ParameterException;
+//import com.sun.org.apache.xpath.internal.operations.String;
+import com.thoughtworks.xstream.XStream;
 import ru.stqa.pft.addressbook.model.GroupData;
 
 import java.io.File;
@@ -10,30 +15,73 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class GroupDataGenerator {
-    public static void main (String[] args) throws IOException {
-        int count = Integer.parseInt(args[0]); //принимаем массив строк и преобразуем в число
-        File file = new File(args[1]);
 
-        List<GroupData> groups = generateGroups(count);
-        save(groups, file);
+    @Parameter (names = "-c", description = "Group count")
+    public int count;
+
+    @Parameter (names = "-f", description = "target file")
+    public String file; //jcommander не поддерживает напрямую работу с файлами
+
+    @Parameter (names = "-d", description = "Data format")
+    public String format;
+
+    public static void main (String[] args) throws IOException {
+        GroupDataGenerator generator = new GroupDataGenerator();
+//        new JCommander(generator, args);
+//        JCommander.newBuilder()
+//                .addObject(generator)
+//                .build()
+//                .parse(args);
+        JCommander jCommander = new JCommander(generator);
+        try {
+            jCommander.parse(java.lang.String.valueOf(args));
+        } catch (ParameterException ex) {
+            jCommander.usage(); //выдаст список доступных опций
+            return;
+        }
+        generator.run();
+//        int count = Integer.parseInt(args[0]); //принимаем массив строк и преобразуем в число
+////        File file = new File(args[1]);
+
+//        List<GroupData> groups = generateGroups(count);
+//        save(groups, file);
 
     }
 
-    private static void save(List<GroupData> groups, File file) throws IOException {
+    private void run() throws IOException {
+        List<GroupData> groups = generateGroups(count);
+        if (format.equals("csv")) {
+            saveAsCsv(groups, new File (String.valueOf(file)));
+        } else if (format.equals("xml")){
+            saveAsXml(groups, new File (java.lang.String.valueOf(file)));
+        } else {
+            System.out.println("unrecognised format" + format);
+        }
+//        save(groups, new File (file));
+    }
+
+    private void saveAsXml(List<GroupData> groups, File file) throws IOException {
+        XStream xstream = new XStream();
+        String xml = xstream.toXML(groups);
+        Writer writer = new FileWriter(file);
+        writer.write(xml);
+        writer.close();
+    }
+
+    private void saveAsCsv(List<GroupData> groups, File file) throws IOException {
 //        System.out.println(new File(".").getAbsolutePath());
         Writer writer = new FileWriter(file);
-        for (GroupData group : groups) {
-            writer.write(String.format("%s,%s,%s\n", group.getName(), group.getHeader(),group.getFooter()));
-        }
+        for (GroupData group : groups)
+            writer.write(java.lang.String.format("%s,%s,%s\n", group.getName(), group.getHeader(), group.getFooter()));
         writer.close();
 
     }
 
-    private static List<GroupData> generateGroups(int count) {
+    private List<GroupData> generateGroups(int count) {
         List<GroupData> groups = new ArrayList<GroupData>();
         for (int i = 0; i < count; i++){
-            groups.add(new GroupData().withName(String.format("test %s", i))
-                    .withHeader(String.format("header %s", i)).withFooter(String.format("footer %s",i)));
+            groups.add(new GroupData().withName(java.lang.String.format("test %s", i))
+                    .withHeader(String.format("header %s", i)).withFooter(java.lang.String.format("footer %s",i)));
         }
         return groups;
     }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -1,23 +1,45 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.Contacts;
+import ru.stqa.pft.addressbook.model.GroupData;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ContactCreationTests extends TestBase{
 
-  @Test (enabled = true)
-  public void testContactCreation() throws Exception {
+  @DataProvider
+  public Iterator<Object[]> validContacts() throws IOException {
+    List<Object[]> list = new ArrayList<Object[]>();
+    BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.csv")));
+    String line = reader.readLine();
+    while (line != null) {
+      String[] split = line.split(",");
+      list.add(new Object[] {new ContactData().withName(split[0]).withSurname(split[1]).
+              withAddress(split[2]).withMobilePhone(split[3]).withEmail(split[4])});
+      line = reader.readLine();
+    }
+    return list.iterator();
+  }
+
+  @Test (dataProvider = "validContacts")
+  public void testContactCreation(ContactData contact) throws Exception {
     Contacts before = app.сontact().all();
 //    Set<ContactData> before = app.сontact().all();
-    File photo = new File("src/test/resources/photo.jpg");
-    ContactData contact = new ContactData().withName("Anna").withSurname("Maksimova").withPhoto(photo).withTitleContact("Contact1")
-            .withTelMobile("9005905555").withEmail("maxann89@gmail.com").withGroup("test4");
+//    File photo = new File("src/test/resources/photo.jpg");
+//    ContactData contact = new ContactData().withName("Anna").withSurname("Maksimova").withPhoto(photo).withTitleContact("Contact1")
+//            .withTelMobile("9005905555").withEmail("maxann89@gmail.com").withGroup("test4");
     app.сontact().create(contact, true);
     app.goTo().homePage();
     assertThat(app.сontact().getContactCount(), equalTo(before.size() + 1));
