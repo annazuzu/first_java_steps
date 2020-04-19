0e4a9a82df83a02320e2419d70d5f30b8e2e79f3
annazuzu <maxann89@gmail.com>
2020-04-19 20:43:37 +0300

    Лекция 10.5. Keyword Driven Testing и инструмент Robot Framework

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
@@ -53,6 +53,10 @@ public class GroupHelper extends HelperBase {
         click(By.name("update"));
     }
 
+    public int count() {
+        return wd.findElements(By.name("selected[]")).size();
+    }
+
     public void create(GroupData group) {
         initGroupCreation();
         fillGroupForm(group);
